2dc7c8b8999e284bb17bb99eb79dce9edce44bf6
Joep Vermaat <joep.vermaat@gmail.com>
2018-11-01 16:28:16 +0100

    Improved env

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # setup drupal
+if [[ ! -f /opt/project/.git ]] && [[ ! -z "$PROJECT_REPO" ]] ; then
+    cd /opt/project/
+    git clone --recurse-submodules $PROJECT_REPO .
+fi
 if ! [[ -e /var/www/html/index.php ]] ; then
     rm -rf /var/www/html
     mkdir /var/www/html
@@ -18,7 +22,7 @@ fi
 if ! [[ -e /usr/local/bin/drush ]] ; then
     cd /tmp && curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer \
     && cd /var/www && composer require drush/drush:^$DRUSH_VERSION \
-    && ln -s /var/www/vendor/drush/drush/drush /usr/local/bin/drush
+    && ln -s /var/www/vendor/drush/drush/drush /usr/local/bin/drush \
 fi
 if ! [[ -e /var/www/html/sites/default/settings.php ]] ; then
     cp /root/config/settings.php /var/www/html/sites/default/settings.php
@@ -35,9 +39,10 @@ if ! [[ -e /var/www/html/sites/default/settings.php ]] ; then
     /bin/sed -i -E "s/@@hash_salt@@/$DRUPAL_MD5/" /var/www/html/sites/default/settings.php
 fi
 cd /var/www/html
-if  [ ! -f /www/html/.installed ] && (! /usr/local/bin/drush status bootstrap | grep -q Successful); then
-    (yes|drush si) && touch /www/html/.installed
+if  [[ ! -f /opt/project/.installed ]] && (! /usr/local/bin/drush status bootstrap | grep -q Successful); then
+    (yes|drush si) && touch /opt/project/.installed
 fi
+drush @none dl registry_rebuild-7.x
 
 # run server
 /usr/sbin/apache2ctl -D FOREGROUND
