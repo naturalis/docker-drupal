5959e81f8037293cf21e9122ca2be19a4ceea7d3
Joep Vermaat <joep.vermaat@gmail.com>
2018-11-01 15:56:47 +0100

    Drush registry rebuild added

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -22,7 +22,8 @@ fi
 if ! [[ -e /usr/local/bin/drush ]] ; then
     cd /tmp && curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer \
     && cd /var/www && composer require drush/drush:^$DRUSH_VERSION \
-    && ln -s /var/www/vendor/drush/drush/drush /usr/local/bin/drush
+    && ln -s /var/www/vendor/drush/drush/drush /usr/local/bin/drush \
+    && drush @none dl registry_rebuild-7.x
 fi
 if ! [[ -e /var/www/html/sites/default/settings.php ]] ; then
     cp /root/config/settings.php /var/www/html/sites/default/settings.php
@@ -39,8 +40,8 @@ if ! [[ -e /var/www/html/sites/default/settings.php ]] ; then
     /bin/sed -i -E "s/@@hash_salt@@/$DRUPAL_MD5/" /var/www/html/sites/default/settings.php
 fi
 cd /var/www/html
-if  [[ ! -f /var/www/html/index.php ]] && (! /usr/local/bin/drush status bootstrap | grep -q Successful); then
-    (yes|drush si)
+if  [[ ! -f /opt/project/.installed ]] && (! /usr/local/bin/drush status bootstrap | grep -q Successful); then
+    (yes|drush si) && touch /opt/project/.installed
 fi
 
 # run server
